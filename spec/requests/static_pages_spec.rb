d115b84a036a8ba83625441bf61945efbc2e7e2b
Ryo YAMAMOTO <dessingens@gmail.com>
2014-06-06 12:31:38 +0900

    use named root and paths

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,49 +6,49 @@ describe "Static Pages" do
 
 	describe "Home Page" do
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit home_path 
 			expect(page).to have_content('Sample App')
 		end
 		it "should have the right title" do
-			visit '/static_pages/home'
+			visit home_path
 			expect(page).to have_title("#{base_title}")
 		end
 		
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit home_path
 			expect(page).not_to have_title('| Home')
 		end
 	end
 
 	describe "Help Page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')
 		end
 		it "should have the right title" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
 	describe "About Page" do
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 		it "should have the right title" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_title("#{base_title} | About Us")
 		end
 	end
 
 	describe "Contact Page" do
 		it "should have the right title" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
 		end
 		it "should have the content 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_content('Contact')
 		end
 	end
