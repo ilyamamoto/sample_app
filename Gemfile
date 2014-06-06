bcb78aa5482ed91d5aeadd3a512e8929493ab92f
Ryo YAMAMOTO <dessingens@gmail.com>
2014-06-07 00:22:20 +0900

    Make a basic User model including secure passwords

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'pg', '0.15.1'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
 
+gem 'bcrypt-ruby', '3.1.2'
+
 group :development, :test do
 	gem 'rspec-rails', '2.13.1'
 	gem 'guard-rspec', '2.1.0'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,18 @@ require 'spec_helper'
 describe User do
 
 	before {
-		@user = User.new(name: "Example User", email: "user@example.com")
+		@user = User.new(name: "Example User", email: "user@example.com",
+			password: "foobar", password_confirmation: "foobar")
 	}
 
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -30,7 +35,7 @@ describe User do
 
 	describe "when email format is invalid" do 
 		it "should be invalid" do
-			addresses = %w[user@foo.com user_at_foo.org example.user@foo.
+			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
 				foo@bar_baz.com foo@bar+baz.com]
 			addresses.each do |invalid_address|
 				@user.email = invalid_address
@@ -41,7 +46,7 @@ describe User do
 
 	describe "when email format is valid" do
 		it "should be valid" do
-			address = %w[ user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+			addresses = %w[ user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
 			addresses.each do |valid_address|
 				@user.email = valid_address
 				expect(@user).to be_valid
@@ -56,6 +61,40 @@ describe User do
 			user_with_same_email.save
 		end
 		it { should_not be_valid }
+	end
+
+	describe "when password is not present" do
+		before do
+			@user = User.new(name: "Example User ", email: "user@example.com",
+				password: " ", password_confirmation: "  ")
+		end
+		it { should_not be_valid }
+	end
+
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
+		it { should_not be_valid }
+	end
+
+	describe "with a password that's too short" do
+		before { @user.password = @user.password_confirmation = "a" * 5 }
+		it { should be_invalid }
+	end
+
+	describe "return value of anthenticate method" do
+		before { @user.save }
+		let(:found_user) { User.find_by(email: @user.email) }
+
+		describe "with valid password" do
+			it { should eq found_user.authenticate(@user.password) }
+		end
+
+		describe "with invalid password" do
+			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+			it { should_not eq user_for_invalid_password }
+			specify { expect(user_for_invalid_password).to be_false }
+		end
 	end
 
 end
