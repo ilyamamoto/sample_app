2b6efb354e7f6388a7272219f061be90345348c0
Ryo YAMAMOTO <dessingens@gmail.com>
2014-06-09 01:18:16 +0900

    add user-update feature

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,9 +34,9 @@ describe "User pages" do
 
 		describe "with valid information" do
 			before do
-				fill_in "Name",				with: "Example User"
-				fill_in "Email", 			with: "user@example.com"
-				fill_in "Password", 	with: "foobar"
+				fill_in "Name",					with: "Example User"
+				fill_in "Email", 				with: "user@example.com"
+				fill_in "Password",		 	with: "foobar"
 				fill_in "Confirmation",	with: "foobar"
 			end
 		
@@ -49,7 +49,7 @@ describe "User pages" do
 				let(:user) { User.find_by(email: 'user@example.com') }
 
 				it { should have_title(user.name) }
-				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+				it { should have_success_message('Welcome') }
 			end
 
 			describe "after saving the user" do
@@ -58,7 +58,7 @@ describe "User pages" do
 				
 				it { should have_link('Sign out') }
 				it { should have_title(user.name) }
-				it { should have_selector('div.alert.alert-success', text: 'Welcome') } 
+				it { should have_success_message('Welcome') } 
 			end
 		end
 
@@ -66,7 +66,10 @@ describe "User pages" do
 
 	describe "edit" do
 		let(:user) { FactoryGirl.create(:user) }
-		before { visit edit_user_path(user) }
+		before { 
+			sign_in user
+			visit edit_user_path(user) 
+		}
 
 		describe "page" do
 			it { should have_content("Update your profile") }
@@ -78,6 +81,24 @@ describe "User pages" do
 			before { click_button "Save changes" }
 
 			it { should have_content('error') }
+		end
+
+		describe "with valid information" do
+			let(:new_name) { "New Name" }
+			let(:new_email) { "new@example.com" }
+			before do
+				fill_in "Name",							with: new_name
+				fill_in "Email",						with: new_email
+				fill_in "Password",					with: user.password
+				fill_in "Confirmation",			with: user.password
+				click_button "Save changes"
+			end
+
+			it { should have_title(new_name) }
+			it { should have_success_message }
+			it { should have_link('Sign out', href: signout_path) }
+			specify { expect(user.reload.name).to  eq new_name }
+			specify { expect(user.reload.email).to eq new_email }
 		end
 	end
 
