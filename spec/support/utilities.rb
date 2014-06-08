80b8f3df65e25915c9ad087665d187a87e439749
Ryo YAMAMOTO <dessingens@gmail.com>
2014-06-09 00:50:25 +0900

    fix how user params are fetched

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -13,7 +13,7 @@ RSpec::Matchers.define :have_error_message do |message|
 end
 
 def sign_in(user, options={})
-#	debugger
+#	binding.pry
 	if options[:no_capybara]
 		remember_token = User.new_remember_token
 		cookies[:remember_token] = remember_token
