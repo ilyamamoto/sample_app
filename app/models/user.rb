3b05e268d25d0a6e2f4430801e833c9dbdc690d8
Ryo YAMAMOTO <dessingens@gmail.com>
2014-06-13 16:50:39 +0900

    add Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+	has_many :reverse_relationships, foreign_key: "followed_id",
+		class_name: "Relationship", dependent: :destroy
+	has_many :followers, through: :reverse_relationships
+
 	has_secure_password # too much powerful: add password & password_confirmation, ensure the existence of password and equality, and the authentication, if the column 'password_digest' exists
 
 	before_save { self.email = email.downcase }
@@ -24,6 +30,18 @@ class User < ActiveRecord::Base
 		#preparatory stage: only with microposts posted by the current_user
 		#the '?' sign needed to escape variable 'id' before inclusion into sql query, preventing sql-injection attacks	
 		Micropost.where("user_id = ?", id)
+	end
+
+	def following?(other_user)
+		relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+		relationships.create!(followed_id: other_user.id)
+	end
+	
+	def unfollow!(other_user)
+		relationships.find_by(followed_id: other_user.id).destroy
 	end
 
 	private
